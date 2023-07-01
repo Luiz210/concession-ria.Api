88f1af187ed54fd67efae658fa56fe03564fa4fa
LUIZ OLIVEIRA <luiz.oliveira159@fatec.sp.gov.br>
2023-07-01 00:34:49 -0300

    Ignorando o ID porque já é incrementado automaticamente então não tenho que ficar excluindo nos testes

--- concessionária/Models/Veiculos.cs
+++ concessionária/Models/Veiculos.cs
@@ -1,7 +1,10 @@
-﻿namespace concessionária.Models
+﻿using System.Text.Json.Serialization;
+
+namespace concessionária.Models
 {
     public class Veiculos
     {
+        [JsonIgnore]
         public int Id { get; set; }
         public required string Placa { get; set; }
         public required string Marca { get; set; }
